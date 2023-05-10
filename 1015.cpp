268cfef3d53ca2299c24140be47cd2abb65e8c4b
zhao-xtao <96565047+zhao-xtao@users.noreply.github.com>
2023-05-10 10:41:47 +0800

    Create 1015.cpp

---
+++ 1015.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+        int resid = 1 % k, len = 1;
+        while (resid) {
+            resid = (resid * 10 + 1) % k;
+            len ++;
+        }
+        return len;
+    }
+};
