5dba219208bd00173919543116c12609d6431d42
zhao-xtao <96565047+zhao-xtao@users.noreply.github.com>
2023-07-11 17:36:11 +0800

    Create 1911.cpp

---
+++ 1911.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        long long even = nums[0], odd = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            even = max(even, odd + nums[i]);
+            odd = max(odd, even - nums[i]);
+        }
+        return even;
+    }
+};
