f70756702be49318064e689169e2849d5d7a48ea
zhao-xtao <96565047+zhao-xtao@users.noreply.github.com>
2023-07-13 12:20:49 +0800

    Create 931.cpp

---
+++ 931.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        copy(matrix[0].begin(), matrix[0].end(), dp[0].begin());
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int mn = dp[i - 1][j];
+                if (j > 0) {
+                    mn = min(mn, dp[i - 1][j - 1]);
+                }
+                if (j < n - 1) {
+                    mn = min(mn, dp[i - 1][j + 1]);
+                }
+                dp[i][j] = mn + matrix[i][j];
+            }
+        }
+        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
+    }
+};
